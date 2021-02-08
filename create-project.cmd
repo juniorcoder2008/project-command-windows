42cd9eb104e52dffe89528f9519635793f956573
Junior Coder <57005033+juniorcoder2008@users.noreply.github.com>
2021-02-08 20:24:33 +0100

    Update create-project.cmd

--- create-project.cmd
+++ create-project.cmd
@@ -1,2 +1,1 @@
-mkdir %1
-git clone https://github.com/juniorcoder2008/dev-env.git
+git clone https://github.com/juniorcoder2008/dev-env.git && ren dev-env %1 && cd %1 && npm install && code . && gulp && npm start 
